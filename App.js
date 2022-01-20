c897c1e35d8d017ad9f119c9a53db9dd45e4215c
unknown <solaiman.sh6@gmail.com>
2022-01-20 16:18:10 +0600

    Initialized app

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+		<>
+			<SafeAreaView style={styles.container}>
+				<StatusBar
+					animated={true}
+					backgroundColor='#E53212'
+					barStyle='default'
+					showHideTransition='slide'
+					hidden={false}
+				/>
+				<Image
+					fadeDuration={1000}
+					source={require('./logo.png')}
+					style={styles.image}
+				/>
+			</SafeAreaView>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#E53212',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		color: '#fff',
+	},
 });
